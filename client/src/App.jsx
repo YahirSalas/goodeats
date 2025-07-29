1f94cf9b97c873122ec014cbbaa050e4571f74fc
YahirSalas <153465207+YahirSalas@users.noreply.github.com>
2025-07-29 10:22:17 -0700

    Fix case-sensitive import for Home.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { supabase } from './supabaseClient'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Login from './pages/Login'
 import Navbar from './components/NavBar'
 import SubmitDeal from './pages/SubmitDeal'
